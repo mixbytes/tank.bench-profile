705a00abd2a8b9317f094e970e7c2358c2a6a0d1
Sergey Govyazin <SGV123@yandex.ru>
2020-01-24 12:31:50 +0300

    V1.0.0 - profile is a separate package from tank.bench-common now

---
+++ lib/index.ts
@@ -0,0 +1,32 @@
+import {
+    CommonConfig,
+    CommonConfigSchema
+} from "./commonConfigSchema";
+import {
+    AnyProfileType,
+    CommitTransactionArgs,
+    ConstructBenchArgs,
+    DestroyBenchArgs,
+    PrepareArgs,
+    Profile,
+    ProfileType,
+    Telemetry,
+    TelemetryData,
+    TransactionResult
+} from "./profile"
+
+
+export {
+    CommonConfig,
+    CommonConfigSchema,
+    AnyProfileType,
+    ProfileType,
+    Telemetry,
+    DestroyBenchArgs,
+    Profile,
+    TelemetryData,
+    PrepareArgs,
+    CommitTransactionArgs,
+    ConstructBenchArgs,
+    TransactionResult
+}
